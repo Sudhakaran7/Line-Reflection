ec624c4dda9752eee00cdcfaf5c8bb3a13829c30
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-08 18:12:49 +0700

    Create line.py

---
+++ line.py
@@ -0,0 +1,27 @@
+import collections
+class Solution(object):
+    def isReflected(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        if not points:
+            return True
+        groups_by_y = collections.defaultdict(set)
+        left, right = float("inf"), float("-inf")
+        for p in points:
+            groups_by_y[p[1]].add(p[0])
+            left, right = min(left, p[0]), max(right, p[0])
+        mid = left + right
+        for group in groups_by_y.values():
+            for x in group:
+                if mid - x not in group:
+                    return False
+        return True
+val=Solution()
+s=int(input())
+mat=[]
+for i in range(s):
+  rows=list(map(int,input().split()))
+  mat.append(rows)
+print(val.isReflected(mat))
